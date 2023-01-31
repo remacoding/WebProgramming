d0d835c47ead865f345e8fb247e6223f577389a6
remacoding <105727870+remacoding@users.noreply.github.com>
2023-01-31 21:21:40 +0200

    Add files via upload

---
+++ ex1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise1</title>
+</head>
+<body>
+    <h1>Write a simple PHP script to print your information (Name and your groupid).</h1>
+<?php
+echo "Reng Majer <br>";
+echo "BBCAP22";
+?>
+<h1>Write PHP code to display the following message. Hello world! My name is "David"</h1>
+<?php
+echo "Hello world! My name is \"Reng\"";
+?>
+<h1>Current Date</h1>
+<?php echo date("d.m.Y") ?>
+</body>
+</html>
